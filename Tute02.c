4d5096680ed7056c512f02c5eb461b4333f5a2a8
it22055576 <127733423+it22055576@users.noreply.github.com>
2023-03-13 20:32:01 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,18 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
-  
+  int x;
+  float price;
+  printf("enter the dictence that the van has traveled :");
+  scanf("%d",&x);
+  if(x>30){
+   price =30*50.0+(x-30)*40.0;
+    printf("the total is:%.2f",price);
+  }
+    else{
+      price=30.0*x;
+      printf("the total price is:%.2f",price);
+    }
+      
   return 0;
 }
